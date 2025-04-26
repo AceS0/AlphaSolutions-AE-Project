36d0f04ee2285654ceb17ae24dee0b7417069061
AceS0 <abse0001@stud.kea.dk>
2025-04-26 14:12:02 +0200

    Initial commit

---
+++ src/main/java/com/example/alphasolutionsaeproject/AlphaSolutionsAeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphasolutionsaeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSolutionsAeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaSolutionsAeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alphasolutionsaeproject/AlphaSolutionsAeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphasolutionsaeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaSolutionsAeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
