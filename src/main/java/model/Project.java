f78af6c78e608f6293867d3ba724e1419e1aca6e
Enes-Lenovo <enfi0001@stud.kea.dk>
2025-04-27 17:05:39 +0200

    Project class done i model

--- src/main/java/model/Project.java
+++ src/main/java/model/Project.java
@@ -1,4 +1,54 @@
 package model;
 
 public class Project {
+
+    private int id;
+    private String title;
+    private String description;
+    private int createdBy;
+
+
+    public Project() {
+    }
+
+
+    public Project(int id, String title, String description, int createdBy) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdBy = createdBy;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
 }
