98957c69f4675783391a0ba93b0b6d77782406c6
Enes-Lenovo <enfi0001@stud.kea.dk>
2025-04-28 09:21:23 +0200

    Alt ift subproject og dens crud funktioner er done. næste step task table

---
+++ src/main/java/service/SubprojectService.java
@@ -0,0 +1,38 @@
+package service;
+
+
+import model.Subproject;
+import org.springframework.stereotype.Service;
+import repository.SubprojectRepository;
+
+import java.util.List;
+
+@Service
+public class SubprojectService {
+
+    private final SubprojectRepository subprojectRepository;
+
+    public SubprojectService(SubprojectRepository subprojectRepository) {
+        this.subprojectRepository = subprojectRepository;
+    }
+
+    public List<Subproject> getAllSubprojects() {
+        return subprojectRepository.findAll();
+    }
+
+    public Subproject getSubprojectById(int id) {
+        return subprojectRepository.findById(id);
+    }
+
+    public void addSubproject(Subproject subproject) {
+        subprojectRepository.save(subproject);
+    }
+
+    public void updateSubproject(Subproject subproject) {
+        subprojectRepository.update(subproject);
+    }
+
+    public void deleteSubproject(int id) {
+        subprojectRepository.delete(id);
+    }
+}
